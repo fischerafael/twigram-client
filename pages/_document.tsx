6fbd87fd8914797a033aea93e4dd1668931af0d4
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-02-09 21:39:29 -0300

    added font rubik

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -31,7 +31,13 @@ export default class MyDocument extends Document {
     render() {
         return (
             <Html lang="pt">
-                <Head></Head>
+                <Head>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;700;900&display=swap"
+                        rel="stylesheet"
+                    />
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
---
+++ src/components/commons/Menu/index.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const Menu = () => {
+    return <MenuWrapperStyle></MenuWrapperStyle>
+}
+
+export default Menu
+
+export const MenuWrapperStyle = styled.nav``
